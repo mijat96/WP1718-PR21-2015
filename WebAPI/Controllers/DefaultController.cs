02840d2a6fef39463b51c652b8d221f9d14d6e83
marko m <Mijat@PC.mijat>
2018-06-17 21:02:15 +0200

    pocetni projekat

---
+++ WebAPI/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace WebAPI.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        //svaki put kad se pokrene vratice se index stranica, pokretanje pocetne stranice
+        [HttpGet, Route("")] //
+        public RedirectResult Index()
+        {
+            var requestUri = Request.RequestUri;    //
+            return Redirect(requestUri.AbsoluteUri + "index.html");
+        }
+    }
+}
