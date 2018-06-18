79c9c69d09d7cb97f62531c97a8100e4a462fccd
marko m <Mijat@PC.mijat>
2018-06-19 01:51:22 +0200

    Model Korisnik, Voznja, Enumeracija.

---
+++ WebAPI/Models/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public enum EnumPol
+    {
+        MUSKO,
+        ZENSKO,
+    }
+
+    public enum EnumUloga
+    {
+        MUSTERIJA,
+        VOZAC,
+        DISPECER,
+    }
+}
---
+++ WebAPI/Models/Korisnik.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Korisnik
+    {
+        public string KorisnickoIme { get; set; }
+        public string Lozinka { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public EnumPol Pol { get; set; }
+        public string JMBG { get; set; }
+        public string KontaktTelefon { get; set; }
+        public string Email { get; set; }
+        public EnumUloga Uloga { get; set; }
+        public List<Voznja> Voznje { get; set; }
+
+        public Korisnik() { }
+    }
+}
