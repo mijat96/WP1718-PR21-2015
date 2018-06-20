032a865c9978c7246fcf06f9279be996a7936a18
marko m <Mijat@PC.mijat>
2018-06-20 03:49:10 +0200

    Logovanje

---
+++ WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public bool Post([FromBody]Korisnik korisnik)
+        {
+            foreach (Musterija kor in Musterije.musterije.Values)
+            {
+                if (kor.KorisnickoIme == korisnik.KorisnickoIme || kor.Lozinka == korisnik.Lozinka)
+                {
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+        
+    }
+}
---
+++ WebAPI/Models/Musterije.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Musterije
+    {
+        public static Dictionary<string, Musterija> musterije { get; set; } = new Dictionary<int, Musterija>();
+
+        public Musterije() { }
+
+        public Musterije(string path)
+        {
+
+            FileStream stream = new FileStream(path, FileMode.Open);
+            StreamReader sr = new StreamReader(stream);
+            EnumPol pol;
+            EnumUloga uloga;
+            string line = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] tokens = line.Split('|');
+                if (tokens[5].Equals("MUSKO"))
+                {
+                    pol = EnumPol.MUSKO;
+                }
+                else
+                {
+                    pol = EnumPol.ZENSKO;
+                }
+                if (tokens[9].Equals("Musterija"))
+                {
+                    uloga = EnumUloga.MUSTERIJA;
+                }
+                else if (tokens[9].Equals("Dispecer"))
+                {
+                    uloga = EnumUloga.DISPECER;
+                }
+                else
+                {
+                    uloga = EnumUloga.VOZAC;
+                }
+                //public Musterija(string kIme, string lozinka, string ime, string prezime, EnumPol pol, string jmbg, string kontakt, string email, EnumUloga uloga)
+                Musterija k = new Musterija(tokens[0], tokens[1], tokens[2], tokens[3], pol, tokens[6], tokens[7], tokens[8], uloga);
+                musterije.Add(k.KorisnickoIme, k);
+            }
+            sr.Close();
+            stream.Close();
+        }
+
+    }
+}
