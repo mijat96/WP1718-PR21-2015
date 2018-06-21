9c8d11c667096cc447eca734dfb70759e3d2a216
marko m <Mijat@PC.mijat>
2018-06-21 04:11:33 +0200

    Stranica sa opcijama korisnika

--- WebAPI/Controllers/LoginController.cs
+++ WebAPI/Controllers/LoginController.cs
@@ -21,6 +21,24 @@ namespace WebAPI.Controllers
 
             }
 
+            foreach (Dispecer d in Dispeceri.dispeceri.Values)
+            {
+                if (d.KorisnickoIme == korisnik.KorisnickoIme || d.Lozinka == korisnik.Lozinka)
+                {
+                    return true;
+                }
+
+            }
+
+            foreach (Vozac v in Vozaci.vozaci.Values)
+            {
+                if (v.KorisnickoIme == korisnik.KorisnickoIme || v.Lozinka == korisnik.Lozinka)
+                {
+                    return true;
+                }
+
+            }
+
             return false;
         }
         
